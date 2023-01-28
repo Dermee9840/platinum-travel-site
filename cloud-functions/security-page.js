beb2c589be185fd9199aaf6c31a5998f03800238
Dermee9840 <iqbeast.9840@gmail.com>
2023-01-29 02:06:22 +0800

    cloud function-iig update hiilee

--- cloud-functions/security-page.js
+++ cloud-functions/security-page.js
@@ -1,4 +1,27 @@
 exports.handler = function (event, context, callback) {
+  const secretContent = `<h3>Хэрэглэгчийн тусгай хуудсанд тавтай морилно уу</h3>
+    <p>Зөвхөн бүртгэлтэй хэрэглэгчдэд зориулсан <strong>хаалттай контентууд.</strong></p>
+  `;
+
+  let body;
+
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if (body.password == "JavaScript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
+
   callback(null, {
     statusCode: 200,
     body: "Хэрэглэгчдийн хуудсанд тавтай морилно уу",
